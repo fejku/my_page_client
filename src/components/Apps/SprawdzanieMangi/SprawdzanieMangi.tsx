27225fa84954b04b72a44655028a83862d1c8718
fejku <przecinek45@o2.pl>
2020-12-20 00:18:36 +0100

    Dodanie spinnera przed ładowaniem listy mang

--- src/components/Apps/SprawdzanieMangi/SprawdzanieMangi.tsx
+++ src/components/Apps/SprawdzanieMangi/SprawdzanieMangi.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import {
   Button,
+  CircularProgress,
   Table,
   TableBody,
   TableCell,
+  TableContainer,
   TableHead,
   TableRow,
 } from "@material-ui/core";
@@ -17,6 +19,7 @@ import { sleepRand } from "../../Common/CommonHelper";
 interface Props {}
 
 const SprawdzanieMangi = (props: Props) => {
+  const [loading, setLoading] = useState(true);
   const [mangi, setMangi] = useState<IManga[]>([]);
   const [dodawanieMangi, setDodawanieMangi] = useState(false);
   const [odswiezanaManga, setOdswiezanaManga] = useState("");
@@ -30,13 +33,12 @@ const SprawdzanieMangi = (props: Props) => {
     const dataMangi: IManga[] = await responseManga.json();
 
     for (const manga of dataMangi) {
-      const responseChapter = await fetch(
-        `/apps/sprawdzanie-mangi/manga/${manga._id}/chaptery`
-      );
+      const responseChapter = await fetch(`/apps/sprawdzanie-mangi/manga/${manga._id}/chaptery`);
       const dataChaptery: IChapter[] = await responseChapter.json();
       manga.chaptery = dataChaptery;
     }
 
+    setLoading(false);
     setMangi(dataMangi);
   };
 
@@ -49,39 +51,34 @@ const SprawdzanieMangi = (props: Props) => {
 
   return (
     <div className={classes.SprawdzanieMangi}>
-      <Button
-        className={classes.SprawdzMangi}
-        variant="outlined"
-        fullWidth
-        onClick={onSprawdzMangiClick}
-      >
+      <Button className={classes.SprawdzMangi} variant="outlined" fullWidth onClick={onSprawdzMangiClick}>
         Sprawdź mangi
       </Button>
-      <Table className={classes.MangiTable}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Nazwa</TableCell>
-            <TableCell>Ostatni przeczytany</TableCell>
-            <TableCell>Najnowszy chapter</TableCell>
-            <TableCell>Ostatnie odświeżenie</TableCell>
-            <TableCell>Akcje</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {mangi.map((manga) => (
-            <MangaItem
-              manga={manga}
-              getMangi={getMangi}
-              odswiezanaManga={odswiezanaManga}
-            />
-          ))}
-        </TableBody>
-      </Table>
+      <TableContainer>
+        <Table className={classes.MangiTable}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nazwa</TableCell>
+              <TableCell>Ostatni przeczytany</TableCell>
+              <TableCell>Najnowszy chapter</TableCell>
+              <TableCell>Ostatnie odświeżenie</TableCell>
+              <TableCell>Akcje</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody className={classes.Test}>
+            {mangi.map((manga) => (
+              <MangaItem manga={manga} getMangi={getMangi} odswiezanaManga={odswiezanaManga} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {loading && (
+        <div className={classes.ProgressWrapper}>
+          <CircularProgress size={68} />
+        </div>
+      )}
 
-      <DodajMange
-        dodawanieState={[dodawanieMangi, setDodawanieMangi]}
-        getMangi={getMangi}
-      />
+      <DodajMange dodawanieState={[dodawanieMangi, setDodawanieMangi]} getMangi={getMangi} />
     </div>
   );
 };
